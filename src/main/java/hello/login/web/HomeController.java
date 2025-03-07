dbf675233ab09112db5ea7df9f63194a46f385f6
khg <rhghdrms100@naver.com>
2025-03-07 17:38:56 +0900

    first commit

---
+++ src/main/java/hello/login/web/HomeController.java
@@ -0,0 +1,42 @@
+package hello.login.web;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final MemberRepository memberRepository;
+
+    //@GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    //required = false -> 로그인 하지 않은 사용자도 들어와야하기 때문
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+
+        if (memberId == null) {
+            return "home";
+        }
+
+        Member loginMember = memberRepository.findById(memberId);
+        //DB에 없거나 종료 되었을 때
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
+    }
+}
